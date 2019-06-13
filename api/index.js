f45a374d3a23c3789d1b3b09a18e62f8b61f5a44
Paceaux <frank.m.taylors08@gmail.com>
2019-06-13 16:56:24 -0500

    uses database directly instead of context

--- api/index.js
+++ api/index.js
@@ -1,31 +1,32 @@
 const typeDefs = require('./typedefs/index');
+const {database, aql} = require('./../setup/database');
 
 const resolvers = {
    Query: {
     getPersonByKey: async function (obj, args, ctx, info) {
         if(args._key == null) return;
 
-        return ctx.database.qNext(ctx.aql`
+        const result =  await database.qNext(aql`
             RETURN DOCUMENT('people', ${args._key})
         `);
+
+        return result;
     }
    },
    Mutation: {
-    upsertPerson: async function (obj, args, ctx, info) {
+    upsertPerson: async function (obj, args, ctxN, info) {
         const record  = args;
 
-        console.log(args);
-        console.log(ctx);
         record.updatedAt = new Date();
 
         if (record._key == null) {
             record.createdAt = new Date();
-            record = await ctx.qNext(ctx.aql`
+            record = await database.qNext(aql`
                 INSERT ${record} INTO people RETURN NEW
             `);
         }
 
-        record = await ctx.qNext(ctx.aql`
+        record = await database.qNext(database.aql`
             UPDATE ${record._key} WITH ${record} IN people RETURN NEW
         `);
 
